137c964818c9fa673aa90e91dcb3014e09841aab
Mahesh-Godela <94672409+Mahesh-Godela@users.noreply.github.com>
2022-08-30 11:15:37 +0530

    Create Smaple.c
     G Mahesh KumarCompleted Sample.c with C source Code

---
+++ OSLAB/Sample.c
@@ -0,0 +1,10 @@
+/* Name Of the Candidate: G.Mahesh Kumar
+   Roll No: 204G1A0551
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
+ #include <stdio.h>
+    void main(){
+	printf("Hello World\n");
+ }
