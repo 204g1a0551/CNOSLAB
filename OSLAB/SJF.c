47df8405b0fc6e3ee6f277a751d05a0b426e3f16
204g1a0551 <94672409+204g1a0551@users.noreply.github.com>
2022-09-06 12:39:41 +0530

    Update SJF.c

--- OSLAB/SJF.c
+++ OSLAB/SJF.c
@@ -1,7 +1,7 @@
 
 /* Name Of the Candidate: G.Mahesh Kumar
    Roll No: 204G1A0551
-   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Title of the Experiment: CPU Scheduling Using SJF Algorithm
    Date of Creation: 06-09-2022
    Date of Execution: 06-09-2022
 */
