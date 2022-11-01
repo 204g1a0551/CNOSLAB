9f4930a2d0aad7d2c73d0a544a1afbf0204ccd81
204g1a0551 <94672409+204g1a0551@users.noreply.github.com>
2022-11-01 12:53:59 +0530

    Update PageReplacement.c

--- OSLAB/PageReplacement.c
+++ OSLAB/PageReplacement.c
@@ -10,9 +10,7 @@ void main()
 {
 void display();
 int i,j,page[12]={2,3,2,1,5,2,4,5,3,2,5,2};
-int
-flag1=0,flag2=0,pf=0,frsize=3,top=0;
-clrscr();
+int flag1=0,flag2=0,pf=0,frsize=3,top=0;
 for(i=0;i<3;i++)
 {
 fr[i]=-1;
